f34559b3dceb9bb7127d6998c86f1a20c35b4eee
Lenora Porter <lporter630@gmail.com>
2018-04-22 21:37:50 -0400

    Adding links to the nav and deleting page-2

---
+++ src/pages/about.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'React';
+
+export default class About extends Component {
+  render() {
+    return (
+      <div>
+        <h1> About Us </h1>
+        <p>Lorizzle ipsizzle fizzle owned amet, sizzle check out this
+        crackalackin. Its fo rizzle tellivizzle velit, shiz yo mamma,
+        suscipizzle doggy, gravida vizzle, we gonna chung. Pellentesque
+        eget tortizzle. Sed erizzle. Yippiyo izzle dolor dapibus boom
+        shackalack tempizzle own yo. Mauris pellentesque nibh izzle
+        tellivizzle. Fizzle izzle crackalackin. Pellentesque eleifend
+        rhoncizzle fizzle. In yo mamma habitasse platea hizzle.
+        Im in the shizzle dapibizzle. Curabitur tellus hizzle, rizzle fo
+        shizzle, boom shackalack fo, eleifend vitae, nunc. Brizzle shizzle my
+         nizzle crocodizzle. Go to hizzle semper velit sizzle purus.
+         </p>
+      </div>
+    )
+  }
+};
