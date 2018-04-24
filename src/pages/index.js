488d16a132d3240528e7bfe27390050a5e21d568
Lenora Porter <lporter630@gmail.com>
2018-04-24 16:25:07 -0400

    Added first post, set custom excerpt, and added html

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,17 @@
 import React from 'react'
 import Link from 'gatsby-link';
+import PostListing from '../components/Posts/PostListing';
 
 
 const IndexPage = ({ data }) => (
   <div>
-    <p>{data.site.siteMetadata.title}</p>
-    <p>{data.site.siteMetadata.desc}</p>
-    {data.allMarkdownRemark.edges.map(({node}) => {
-      return <PostListing post={node} />
-    })}
+    <h2>Writing</h2>
+    <h5>My thoughts, my design process, and career.</h5>
+    {data.allMarkdownRemark.edges.map(({node}) => (
+     <PostListing key={node.id} post={node} />
+   ))}
   </div>
 );
-
-const PostListing = () => <div> Hello </div>;
 
 export default IndexPage
 
@@ -27,11 +26,13 @@ export const query = graphql `
     allMarkdownRemark {
    edges {
      node {
+       id
        frontmatter {
          title
          date(formatString: "MMMM DD YYYY")
        }
        html
+       excerpt(pruneLength: 280)
      }
    }
  }
